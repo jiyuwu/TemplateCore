3d7fde16980c10e8bdea1740e94b1332d22b218f
yincihang <923374829@qq.com>
2019-11-11 17:38:46 +0800

    部分二维码模块

---
+++ Common/QRCoderHelper.cs
@@ -0,0 +1,58 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Common
+{
+  public  class QRCoderHelper
+    {
+        #region 普通二维码
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url">存储内容</param>
+        /// <param name="pixel">像素大小</param>
+        /// <returns></returns>
+        public static Bitmap GetPTQRCode(string url, int pixel)
+        {
+            QRCodeGenerator generator = new QRCodeGenerator();
+            QRCodeData codeData = generator.CreateQrCode(url, QRCodeGenerator.ECCLevel.M, true);
+            QRCoder.QRCode qrcode = new QRCoder.QRCode(codeData);
+            Bitmap qrImage = qrcode.GetGraphic(pixel, Color.Black, Color.White, true);
+            return qrImage;
+        }
+        #endregion
+
+        #region 带logo的二维码
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url">存储内容</param>
+        /// <param name="pixel">像素大小</param>
+        /// <returns></returns>
+        public static Bitmap GetLogoQRCode(string url,string logoPath, int pixel)
+        {
+            QRCodeGenerator generator = new QRCodeGenerator();
+            QRCodeData codeData = generator.CreateQrCode(url, QRCodeGenerator.ECCLevel.M, true);
+            QRCoder.QRCode qrcode = new QRCoder.QRCode(codeData);
+            Bitmap icon = new Bitmap(logoPath);
+            Bitmap qrImage = qrcode.GetGraphic(pixel, Color.Black, Color.White, icon,15,6, true);
+            #region 参数介绍
+            //GetGraphic方法参数介绍
+            //pixelsPerModule //生成二维码图片的像素大小 ，我这里设置的是5
+            //darkColor       //暗色   一般设置为Color.Black 黑色
+            //lightColor      //亮色   一般设置为Color.White  白色
+            //icon             //二维码 水印图标 例如：Bitmap icon = new Bitmap(context.Server.MapPath("~/images/zs.png")); 默认为NULL ，加上这个二维码中间会显示一个图标
+            //iconSizePercent  //水印图标的大小比例 ，可根据自己的喜好设置
+            //iconBorderWidth  // 水印图标的边框
+            //drawQuietZones   //静止区，位于二维码某一边的空白边界,用来阻止读者获取与正在浏览的二维码无关的信息 即是否绘画二维码的空白边框区域 默认为true
+            #endregion
+            return qrImage;
+        }
+        #endregion
+
+
+    }
+}
---
+++ TemplateCore/Controllers/QRCodeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateCore.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        //private readonly IHostingEnvironment _hostingEnvironment; 3.0之前使用它
+        public QRCodeController(IWebHostEnvironment _webHostEnvironment)
+        {
+            webHostEnvironment=_webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public void GetPTQRCode(string url, int pixel=5)
+        {
+            url = HttpUtility.UrlDecode(url);
+            Response.ContentType = "image/jpeg";
+
+            var bitmap = QRCoderHelper.GetPTQRCode(url, pixel);
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Jpeg);
+
+            Response.Body.WriteAsync(ms.GetBuffer(), 0, Convert.ToInt32(ms.Length));
+            Response.Body.Close();
+        }
+        public void GetLogoQRCode(string url,string logoPath, int pixel = 5)
+        {
+            url = HttpUtility.UrlDecode(url);
+            logoPath= webHostEnvironment.WebRootPath + HttpUtility.UrlDecode(logoPath);
+            Response.ContentType = "image/jpeg";
+
+            var bitmap = QRCoderHelper.GetLogoQRCode(url, logoPath, pixel);
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Jpeg);
+
+            Response.Body.WriteAsync(ms.GetBuffer(), 0, Convert.ToInt32(ms.Length));
+            Response.Body.Close();
+        }
+    }
+}
